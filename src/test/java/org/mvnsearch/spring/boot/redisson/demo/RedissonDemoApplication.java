83c2cc7e7238e1e8e1cd5725b26621fb128935c7
linux_china <linux_china@hotmail.com>
2016-05-09 23:52:28 +0800

    redisson demo application

---
+++ src/test/java/org/mvnsearch/spring/boot/redisson/demo/RedissonDemoApplication.java
@@ -0,0 +1,17 @@
+package org.mvnsearch.spring.boot.redisson.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * redisson demo application
+ *
+ * @author linux_china
+ */
+@SpringBootApplication
+public class RedissonDemoApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(RedissonDemoApplication.class, args);
+    }
+
+}
