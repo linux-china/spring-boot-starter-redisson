0d27a5a5d86c3b371f0eebcf2e55205645ec8627
linux_china <linux_china@hotmail.com>
2017-03-27 14:41:05 +0800

    ajust because of api changed

--- src/main/java/org/mvnsearch/spring/boot/redisson/RedissonAutoConfiguration.java
+++ src/main/java/org/mvnsearch/spring/boot/redisson/RedissonAutoConfiguration.java
@@ -1,6 +1,10 @@
 package org.mvnsearch.spring.boot.redisson;
 
 import org.redisson.*;
+import org.redisson.api.RedissonClient;
+import org.redisson.config.Config;
+import org.redisson.config.SentinelServersConfig;
+import org.redisson.config.SingleServerConfig;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.AutoConfigureAfter;
 import org.springframework.boot.autoconfigure.data.redis.RedisAutoConfiguration;
